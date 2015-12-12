a01a8b13e7095b8c629746a831dbc820b5fb0670
hmln <helio@meiralins.org>
2015-12-12 02:45:42 -0300

    list2 ok

--- basic/list2.py
+++ basic/list2.py
@@ -13,18 +13,63 @@
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
+def _remove_adjacent_gen(list):
+    iterator = iter(list)
+    last = next(iterator)
+    yield last
+    while True:
+        n = next(iterator)
+        while n == last:
+            n = next(iterator)
+        yield n
+        last = n
+
+
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+    return list(_remove_adjacent_gen(nums))
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
+def _merge_gen(list1, list2):
+    it1 = iter(list1)
+    it2 = iter(list2)
+
+    # accepts empty lists
+    try:
+        n1 = next(it1)
+    except StopIteration:
+        yield from it2
+        return
+    try:
+        n2 = next(it2)
+    except StopIteration:
+        yield n1
+        yield from it1
+        return
+
+    while True:
+        if n1 < n2:
+            yield n1
+            try:
+                n1 = next(it1)
+            except StopIteration:
+                yield n2
+                yield from it2
+                return
+        else:
+            yield n2
+            try:
+                n2 = next(it2)
+            except StopIteration:
+                yield n1
+                yield from it1
+                return
+
 def linear_merge(list1, list2):
-    # +++your code here+++
-    return
+    return list(_merge_gen(list1, list2))
 
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
@@ -61,6 +106,10 @@ def main():
          ['aa', 'bb', 'cc', 'xx', 'zz'])
     test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
          ['aa', 'aa', 'aa', 'bb', 'bb'])
+    test(linear_merge([], ['aa', 'bb', 'bb']),
+         ['aa', 'bb', 'bb'])
+    test(linear_merge(['aa', 'bb', 'bb'], []),
+         ['aa', 'bb', 'bb'])
 
 
 if __name__ == '__main__':
